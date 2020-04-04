75d2c4040416ba4f31a167ccfff60e785e657ab4
kxgh <fractal.kaktus@protonmail.com>
2020-04-04 18:38:44 +0200

    no permission exception when attempting to stat file unauth'd

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-## kx-file-lister-sync
+# kx-file-lister-sync
 
-Content listing in directories using Node.js. Supporting recursion, detailed output, blacklists, whitelists, size formatters.
-Synchronous only.
+Content listing in directories using Node.js. Supporting recursion, stats, detailed output, blacklists, whitelists, size formatters.
+Fully synchronous.
 
 ## Example
 
@@ -9,14 +9,16 @@ Basic usage:
 
 ```javascript
 const fl = new (require('kx-file-lister-sync'))();
-const list = fl.listFiles('../some/folder', true); // true for RECURSIVE
+const list = fl.listFiles('../some/folder');
 console.log(list)
 ```
 Output:
 ```
 [ '..\\some\\backgorund.js',
   '..\\some\\manifest.js',
-  '..\\some\\docs\\cv.ods' ]
+  '..\\some\\docs\\cv.ods',
+  '..\\some\\docs\\photos\\pic.jpg',
+  '..\\some\\docs\\photos\\pic2.jpg' ]
 ```
 
 More examples at the bottom.
@@ -24,8 +26,8 @@ More examples at the bottom.
 ## API
 
 ```javascript
-const KFLS = require('kx-file-lister-sync');
-let fl = new KFLS(opts);
+const FileLister = require('kx-file-lister-sync');
+let fl = new FileLister(opts);
 ```
 
 ### Constructor
@@ -38,42 +40,51 @@ Object opts is passed to contructor. It accepts these properties:
 
 #### detailed
 
-If set to `true` it will list objects with properties listed in `details` params. `false` for plain string filepaths.
-Defaults to `false`
+If set to `true` it will list objects with properties
+`name` for file name,
+`path` for file path
+`full` for full file path,
+`ext` for extension and
+`dir` for whether it is a directory.
+Otherwise it returns plain string filepaths. Defaults to `false`
 
-#### details
+#### stats
 
-Either `'all'` or an array of strings. Each string represents which detail will be returned along with full path, path and file name.
-These 3 properties are always present. Refer to stat function of 'fs' module for more options. Defaults to `['size']`
+Enables detailed mode and each item will also include fs stats. Contains single err property, if could not stat.
+Defaults to `false`.
 
 #### blackList
 
-Array of strings or `RegExp`s, or both. Each file will be checked by each blackList entry if it matches it will not be listed.
-Matching is done by `String.prototype.endsWith` function, while regexps are checked by `RegExp.prototype.test`.
-Only works if whiteList is empty or null. Defaults to `[]`
+Array of strings or `RegExp`s, or both. Each (non-dir) file will be checked by each blackList entry if it matches it will not be listed.
+Matching is done by `String.prototype.endsWith` function on file's name, while regexps are checked by `RegExp.prototype.test`.
+Only works if whiteList is empty or missing. Defaults to `[]`
 
 #### whiteList
 
 Does the opposite of what blackList does. If whiteList is specified and has at least one element, all listings ignore blackList
 and use whiteList instead. Empty by default.
 
+#### filterDirs
+
+White and black lists also affect diving into directories if set to `true`. Defaults to `false`.
+
 #### sizeFormatter
 
 Value of this property must be a function and should take one `Number` param (file size in bytes) and output anything.
 Decorator for file size.
-
-#### showHidden
-
-Does not output entries that start with period. Defaults to `false`.
 
 #### sortByName
 
 Whether to sort output entries by name. If set to `false` (default) it will sort output entries by 'directories first'.
 Defaults to `false`.
 
+#### ignoreLinks
+
+Output will not contain files that are symbolic links. Defaults to `true`.
+
 ### listFiles(dirPath[, recursive, maxDepth])
 
-Lists all files in directories and possibly subdirectories.
+Lists all files in directories and possibly subdirectories into a single flat array.
   * `dirPath` target directory path
   * `recursive` also lists subdirectories if `true`. Defaults to `true`
   * `maxDepth` do not dive into subdirectories deeper than this number. 0 for just target directory, 1 for target directory
@@ -81,7 +92,7 @@ Lists all files in directories and possibly subdirectories.
 
 ### listOneDir(dirPath)
 
-Lists files AND DIRECTORIES in target directory.
+Lists files **and directories** in target directory.
 
   * `dirPath` target directory path
 
@@ -94,11 +105,11 @@ Lists files into a deep array, mapping fs's tree structure.
   
 ## Which methods are recursive?
 
-| Method        | Recursive?    |
-| ------------- |:-------------:|
-| listFiles     | yes, settable |
-| listOneDir    | no            |
-| listFilesTree | yes           |
+| Method        | Recursive?    | Lists dirs?   |
+| ------------- |:-------------:|:-------------:|
+| listFiles     | yes, settable | no            |
+| listOneDir    | no            | yes           |
+| listFilesTree | yes           | as a structure|
 
   
 ## Examples
@@ -106,75 +117,74 @@ Lists files into a deep array, mapping fs's tree structure.
 Code:
 
 ```javascript
-const KFLS = require('kx-file-lister-sync');
-const fl = new KFLS({
+const FileLister = require('kx-file-lister-sync');
+const fl = new FileLister({
     detailed: true,
     sortByName: true,
     whiteList: [/m[ao]/,'.js']
 });
-const result = fl.listFiles('someDir');
+const result = fl.listFiles('xx/some');
 console.log(result);
 ```
 
 Outputs to:
 ```
-[ { dir: false,
-    name: 'backgorund.js',
-    path: 'someDir\\4money.js',
-    full: 'C:\\Users\\kxgh\\projs\\some\\backgorund.js',
-    size: 4096 },
-  { dir: false,
-    name: 'manifest.js',
-    path: 'someDir\\manifest.js',
-    full: 'C:\\Users\\kxgh\\projs\\some\\manifest.js',
-    size: 546861 } ]
+[ { name: 'backgorund.js',
+    path: 'xx\\some\\backgorund.js',
+    dir: false,
+    ext: '.js',
+    full:
+     'C:\\Users\\kxgh\\projs\\kx-file-lister-sync\\xx\\some\\backgorund.js' },
+  { name: 'manifest.js',
+    path: 'xx\\some\\manifest.js',
+    dir: false,
+    ext: '.js',
+    full:
+     'C:\\Users\\kxgh\\projs\\kx-file-lister-sync\\xx\\some\\manifest.js' } ]
 ```
 ---
 
 Code:
 
 ```javascript
-const KFLS = require('kx-file-lister-sync');
-const fl = new KFLS();
-const result = fl.listFilesTree('ourTarget');
+const FileLister = require('kx-file-lister-sync');
+const fl = new FileLister({blackList: ['some'], filterDirs: true});
+const result = fl.listFilesTree('xx');
 console.log(result);
 ```
 
 Outputs to:
 ```
-[ [ [ 'ourTarget\\docs\\photos\\pic.jpg',
-      'ourTarget\\docs\\photos\\pic2.jpg' ],
-    'ourTarget\\docs\\cv.ods' ],
-  'ourTarget\\backgorund.js',
-  'ourTarget\\manifest.js' ]
+[ [ [ 'xx\\xy\\xz\\ac.js' ], 'xx\\xy\\ab.js' ], 'xx\\aa.js' ]
 ```
 ---
 
 Code:
 
 ```javascript
-const KFLS = require('kx-file-lister-sync');
-const fl = new KFLS({
+const FileLister = require('kx-file-lister-sync');
+const fl = new FileLister({
     details: ['size', 'birthtime'],
-    blackList: [/manifest/i]
+    whiteList: [/manifest/i],
+    detailed: true
 });
-const result = fl.listOneDir('ourTarget');
+const result = fl.listOneDir('xx/some');
 console.log(result);
 ```
 
 Outputs to:
 
 ```
-[ { dir: true,
-    name: 'docs',
-    path: 'ourTarget\\docs',
-    full: 'C:\\Users\\kxgh\\projs\\ourTarget\\docs',
-    size: 0,
-    birthtime: 2020-04-03T18:37:02.426Z },
-  { dir: false,
-    name: 'backgorund.js',
-    path: 'ourTarget\\backgorund.js',
-    full: 'C:\\Users\\kxgh\\projs\\ourTarget\\backgorund.js',
-    size: 2224,
-    birthtime: 2020-04-03T18:36:40.349Z } ]
+[ { name: 'docs',
+    path: 'xx\\some\\docs',
+    dir: true,
+    ext: '',
+    full:
+     'C:\\Users\\kxgh\\projs\\kx-file-lister-sync\\xx\\some\\docs' },
+  { name: 'manifest.js',
+    path: 'xx\\some\\manifest.js',
+    dir: false,
+    ext: '.js',
+    full:
+     'C:\\Users\\kxgh\\projs\\kx-file-lister-sync\\xx\\some\\manifest.js' } ]
 ```
--- index.js
+++ index.js
@@ -2,55 +2,90 @@ const fs = require('fs');
 const path = require('path');
 
 function getLogic(opts) {
-    const lg = {
-        statFile(filePath) {
-            const stat = fs.statSync(path.resolve(filePath));
-            const o = opts.details == 'all' ? {...stat} : {};
-            o.dir = stat.isDirectory();
-            o.name = path.basename(filePath);
-            o.path = path.normalize(filePath);
-            o.full = path.resolve(o.path);
+    class Dc {
+        constructor(dpath, direntEntryList) {
+            this.path = path.normalize(dpath);
+            this.list = direntEntryList;
+        }
 
-            if (opts.details != 'all')
-                for (let d of opts.details)
-                    o[d] = stat[d];
-            if (opts.sizeFormatter)
-                o.size = opts.sizeFormatter(o.size);
-            return o
-        },
-        sortByDir(content) {
+        /**
+         * @param {boolean} filterDirs
+         * @returns {Dc} this
+         */
+        filterContent(filterDirs) {
+            if (filterDirs)
+                this.list = this.list.filter(de => (!de.isSymbolicLink() || !opts.ignoreLinks) && lg.pass(de.name));
+            else
+                this.list = this.list.filter(de => de.isDirectory() || ((!de.isSymbolicLink() ||
+                    !opts.ignoreLinks) && lg.pass(de.name)));
+            return this
+        }
+
+        /**
+         * @returns {Dc} this
+         */
+        sortByDir() {
             if (opts.sortByName)
-                return content;
-            let dirs = [];
-            let files = [];
-            for (let c of content)
-                lg.isDir(c) ? dirs.push(c) : files.push(c);
-            return dirs.concat(files)
-        },
-        simplify(content, recursive) {
-            if (!opts.detailed) {
-                if (recursive) {
-                    for (let i in content) {
-                        if (Array.isArray(content[i]))
-                            content[i] = lg.simplify(content[i], true);
-                        else content[i] = content[i].path;
+                return this;
+            const dirs = [],
+                files = [];
+            for (let de of this.list)
+                de.isDirectory() ? dirs.push(de) : files.push(de);
+            this.list = dirs.concat(files);
+            return this
+        }
+
+        /**
+         * @returns {Object[]|string[]}
+         */
+        finalizeContent() {
+            const fmapper = e => {
+                if (e instanceof Dc) {
+                    return e.finalizeContent();
+                } else if (opts.detailed) {
+                    const o = {
+                        name: path.basename(e.name),
+                        path: e.path,
+                        dir: e.isDirectory()
+                    };
+                    o.ext = path.extname(o.name);
+                    o.full = path.resolve(o.path);
+                    if (opts.stats) {
+                        try {
+                            o.stats = fs.statSync(o.full)
+                            if(opts.sizeFormatter)
+                                o.stats.size = opts.sizeFormatter(opts.stats.size)
+                        } catch (err) {
+                            o.stats = {err: 'NO PERMISSION'}
+                        }
                     }
-                    return content
-                } else return content.map(o => o.path)
-            }
-            return content
-        },
+                    return o
+                } else return e.path
+            };
+            return this.list.map(fmapper);
+        }
+    }
+
+    const lg = {
+        /**
+         * @param {string} dirPath
+         * @returns {Dc}
+         */
         getDirContent(dirPath) {
-            return fs.readdirSync(dirPath).map(name => lg.statFile(path.join(dirPath, name)));
+            try {
+                const dc = fs.readdirSync(dirPath, {withFileTypes: true});
+                dc.forEach(c => c.path = path.join(dirPath, c.name));
+                return new Dc(dirPath, dc)
+            } catch (err) {
+                return new Dc(dirPath, [])
+            }
         },
-        filterContent(content, dirsPass) {
-            if (dirsPass)
-                return content.filter(o => o.dir || lg.pass(o.name));
-            return content.filter(o => lg.pass(o.name));
-        },
+
+        /**
+         * @param {string} fileName
+         * @returns {boolean}
+         */
         pass(fileName) {
-            if (!opts.showHidden && fileName[0] === '.')
-                return false;
             let ok = opts.useList === 'b';
             if (ok) {
                 for (let li of opts.blackList) {
@@ -69,65 +104,83 @@ function getLogic(opts) {
             }
             return ok
         },
+        /**
+         *
+         * @param {string} dirPath
+         * @returns {Dc}
+         */
+        listOneDir(dirPath) {
+            return lg.getDirContent(dirPath).filterContent(opts.filterDirs).sortByDir()
+        },
+        /**
+         * @param dirPath
+         * @param recursive
+         * @param maxDepth
+         * @param depth
+         * @returns {Dc}
+         */
         listFiles(dirPath, recursive, maxDepth, depth) {
-            const content = lg.filterContent(lg.getDirContent(dirPath), true);
-            const files = [];
-            const dirs = [];
-            for (let c of content) {
-                if (lg.isDir(c))
-                    dirs.push(c);
-                else files.push(c);
+            const dc = lg.getDirContent(dirPath).filterContent(opts.filterDirs); // will be sorted anyway .sortByDir();
+            const fDes = [];
+            const dDes = [];
+            for (let c of dc.list) {
+                if (c.isDirectory())
+                    dDes.push(c);
+                else fDes.push(c);
             }
             if (recursive && (maxDepth === -1 || maxDepth > depth))
-                dirs.forEach(dir => {
-                    files.push(...lg.listFiles(dir.path, recursive, maxDepth, (depth + 1)))
+                dDes.forEach(dDe => {
+                    fDes.push(...lg.listFiles(dDe.path, recursive, maxDepth, (depth + 1)).list)
                 });
-            return files;
+            dc.list = fDes;
+            return dc
         },
-        listDir(dirPath) {
-            return lg.simplify(lg.sortByDir(lg.filterContent(lg.getDirContent(dirPath), false)));
-        },
-        isDir(f) {
-            if (typeof f === 'string')
-                return fs.statSync(f).isDirectory();
-            return !!f.dir
-        },
+        /**
+         * @param {string} dirPath
+         * @param {number} maxDepth
+         * @param {number} depth
+         * @returns {Dc}
+         */
         listTree(dirPath, maxDepth, depth) {
-            const content = lg.sortByDir(lg.filterContent(lg.getDirContent(dirPath), true));
-            if (maxDepth === -1 || maxDepth > depth)
-                for (let i in content) {
-                    if (lg.isDir(content[i]))
-                        content[i] = lg.listTree(content[i].path, maxDepth, (depth + 1));
+            const dc = lg.getDirContent(dirPath).filterContent(opts.filterDirs).sortByDir();
+            for (let i in dc.list) {
+                if (dc.list[i].isDirectory()) {
+                    if (maxDepth === -1 || maxDepth > depth) {
+                        dc.list[i] = lg.listTree(dc.list[i].path, maxDepth, (depth + 1));
+                    }
                 }
-            return content
+            }
+            return dc
         }
     };
     return lg
 }
 
-module.exports = class KFLS {
+module.exports = class FileLister {
     /**
      * Create a synchronous file lister.
-     * @param {Object} opts options for file listing.
+     * @param {Object} opts options for file listing
      * @param {boolean} [opts.detailed=false] true for detailed output. Detailed output will generate array of objects, not strings
-     * @param {string[]|string} [opts.details=['size']] array of fs.stat details returned. 'all' for everything
+     * @param {boolean} [opts.stats=false] set true for details + file stats
      * @param {string[]|RegExp[]} [opts.blackList=[]] files with names ending with strings in blackList will not be listed
      * @param {string[]|RegExp[]} [opts.whiteList=[]] only files with names in whiteList will be listed. BlackList will be ignored if present.
+     * @param {boolean} [opts.filterDirs] whether black/white list should also filter directories
      * @param {Function} [opts.sizeFormatter=null] returned sizes will be passed through this function
-     * @param {boolean} [opts.showHidden=false] blacklists .dot files
      * @param {boolean} [opts.sortByName=false] sorts lists so that directories are always first if set to true
+     * @param {boolean} [opts.ignoreLinks=true] filters out symbolic links
      *
      */
     constructor(opts = {}) {
         this.opts = opts;
-        this.opts.detailed = Boolean(opts.detailed || (opts.details && opts.details.length));
-        this.opts.details = opts.details || ['size'];
         this.opts.blackList = opts.blackList || [];
+        this.opts.stats = !!opts.stats;
+        this.opts.detailed = !!opts.detailed || this.opts.stats;
+        this.opts.filterDirs = !!opts.filterDirs;
         this.opts.whiteList = opts.whiteList || [];
         this.opts.useList = this.opts.whiteList.length ? 'w' : 'b';
         this.opts.sizeFormatter = opts.sizeFormatter;
-        this.opts.showHidden = Boolean(opts.showHidden);
         this.opts.sortByName = Boolean(opts.sortByName);
+        this.opts.ignoreLinks = opts.ignoreLinks !== false;
         this._lg = getLogic(this.opts)
     }
 
@@ -140,10 +193,7 @@ module.exports = class KFLS {
      */
     listFiles(dirPath, recursive = true, maxDepth = -1) {
         dirPath = path.normalize(dirPath);
-        maxDepth = parseInt(maxDepth);
-        if (this._lg.isDir(dirPath) && !isNaN(maxDepth))
-            return this._lg.simplify(this._lg.listFiles(dirPath, recursive, maxDepth, 0));
-        return null
+        return this._lg.listFiles(dirPath, recursive, maxDepth, 0).finalizeContent();
     }
 
     /**
@@ -153,27 +203,24 @@ module.exports = class KFLS {
      */
     listOneDir(dirPath) {
         dirPath = path.normalize(dirPath);
-        if (!this._lg.isDir(dirPath))
-            return [];
-        return this._lg.listDir(dirPath);
+        return this._lg.listOneDir(dirPath).finalizeContent();
     }
 
     /**
      * Lists files into a deep array, copying fs's tree structure.
      * @param {string} dirPath target directory path
      * @param {number} [maxDepth] do not dive into subdirectories deeper than maxDepth
-     * @returns {Object[]}
+     * @returns {string[]|Object[]}
      */
     listFilesTree(dirPath, maxDepth = -1) {
-        return this._lg.simplify(this._lg.listTree(dirPath, maxDepth, 0), true);
+        return this._lg.listTree(dirPath, maxDepth, 0).finalizeContent()
     }
 
-    /**
-     * Checks whether the filePath is a directory.
-     * @param {string|Object} filePath fs will be asked if filePath is string, otherwise its 'dir' property will be returned
-     * @returns {boolean} whether it is a directory
-     */
-    isDirectory(filePath) {
-        return this._lg.isDir(filePath)
+    isDirectory(filePath){
+        try{
+            return fs.statSync(filePath).isDirectory()
+        }catch(err){
+            return false;
+        }
     }
 };
